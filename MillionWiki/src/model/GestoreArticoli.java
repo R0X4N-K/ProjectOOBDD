080bf72cedf1bac96cb8ee3d977ca3fe8f9a9485
roxan <giovannizapparella002@gmail.com>
2023-11-29 17:14:56 +0100

    definita classe statica GestoreArticoli

--- MillionWiki/src/model/GestoreArticoli.java
+++ MillionWiki/src/model/GestoreArticoli.java
@@ -1,4 +1,36 @@
 package model;
 
-public class GestoreArticoli {
+import java.util.ArrayList;
+
+public final class GestoreArticoli {
+    private GestoreArticoli(){
+
+    }
+
+    public static Articolo fornisciArticolo(String nomeArticolo) {
+        return null; //momentaneamente ritorna null per evitare errori
+    }
+
+    public static VersioneArticolo fornisciVersioneArticolo(String nomeArticolo) {
+        return null; //momentaneamente ritorna null per evitare errori
+    }
+
+    public static ArrayList<VersioneArticolo> notificaAutore() {
+        return null; //momentaneamente ritorna null per evitare errori
+    }
+
+    public static void memorizzaArticolo(Articolo articoloDaMemorizzare){
+
+    }
+
+    /*
+    CONTROLLO PLAGIO  - DA IMPLEMENTARE
+    public static void controllaValiditaArticolo(Articolo articoloDaValidare){
+
+    }
+
+    public static void controllaValiditaArticolo(VersioneArticolo versioneArticoloDaValidare){
+
+    }
+    */
 }
