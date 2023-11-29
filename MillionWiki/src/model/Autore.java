1a107813fd2d6f78988d9f5cbe88076bf6b0c4b7
Sarnyxq <gennaro.sarnataro8@gmail.com>
2023-11-29 16:13:33 +0100

    Creazione classi in model e inserimento attributi ad Autore

---
+++ MillionWiki/src/model/Autore.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Autore extends Utente {
+    private String nomeUtente;
+    private String password;
+    private float valutazione;
+
+    private ArrayList <VersioneArticolo>
+
+
+
+
+
+}
