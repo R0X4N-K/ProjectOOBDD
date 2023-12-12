8254d5323dc49a77c54bc3feb0c3c6d64d753641
MaxDamageDevStuff <68134538+MaxDamageDevStuff@users.noreply.github.com>
2023-12-12 16:44:05 +0100

    Modificate set di password e nome utente
    la modifica consiste nell'inserimento di un controllo sulla validità della stringa e l'aggiunta di un exception

--- MillionWiki/src/model/Autore.java
+++ MillionWiki/src/model/Autore.java
@@ -7,21 +7,33 @@ public class Autore extends Utente {
     private String password;
     private float valutazione;
 
+    public Autore (String nomeUtente, String password) throws Exception {
+
+        setNomeUtente(nomeUtente);
+
+        setPassword(password);
+    }
 
     public String getNomeUtente(){
         return nomeUtente;
     }
 
-    public void setNomeUtente(String nome){
-
-        this.nomeUtente = nome;
+    public void setNomeUtente(String nome) throws Exception{
+        if (!nomeUtente.isEmpty() && !password.isBlank()) {
+            this.nomeUtente = nome;
+        } else {
+            throw new Exception("NOME UTENTE VUOTO!"); // TODO: creare eccezione ad hoc
+        }
     }
 
 
 
-    public void setPassword(String password){
-
-        this.password= password;
+    public void setPassword(String password) throws Exception{
+        if (!password.isEmpty() && !password.isBlank()) {
+            this.password = password;
+        } else {
+            throw new Exception("PASSWORD VUOTA!"); // TODO: creare eccezione ad hoc
+        }
     }
 
 
@@ -31,7 +43,7 @@ public class Autore extends Utente {
         return valutazione;
     }
 
-    public void setValutazione(float valutazione) {
+    public void setValutazione (float valutazione) {
         this.valutazione = valutazione;
     }
 
