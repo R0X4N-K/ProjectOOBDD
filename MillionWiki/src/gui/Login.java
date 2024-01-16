9527af8ab679801be80ee07b172721da1c863d4a
Sarnyxq <151854412+Sarnyxq@users.noreply.github.com>
2024-01-16 13:27:23 +0100

    Commit dell'implementazione dell'interfaccia Login ancora pero' da testare

---
+++ MillionWiki/src/gui/Login.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class Login {
+    private JTextArea entraTextArea;
+    private JTextArea loginTextArea;
+    private JTextField textField1;
+    private JTextArea passwordTextArea;
+    private JPasswordField passwordField1;
+    private JButton Entra;
+    private JButton NonRegistrato;
+}
