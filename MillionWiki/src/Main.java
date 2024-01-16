49564207570ef78eddc3027aa8483ad130e585ec
R0X4N-K <giovannizapparella002@gmail.com>
2024-01-16 17:31:37 +0100

    rimozione del main in Home, aggiunta del mainPanel in getContentPane

--- MillionWiki/src/Main.java
+++ MillionWiki/src/Main.java
@@ -1,9 +1,12 @@
+import gui.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
         //ystem.out.printl("Hello MOtherfucker");
+        new Home();
 
     }
 }
--- MillionWiki/src/gui/Home.java
+++ MillionWiki/src/gui/Home.java
@@ -5,25 +5,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Home {
-    private JButton Accedi;
+
+
+    private JButton accedi;
+    private JButton registratiButton;
+    private JPanel mainPanel;
 
     public Home() {
-        Accedi.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame frame = new JFrame("Home");
-                frame.setContentPane(new Home().Accedi);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
-            }
-        });
-    }
-    public static void main(String[] args) {
         JFrame frame = new JFrame("Home");
-        frame.setContentPane(new Home().Accedi);
+        frame.setContentPane(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        accedi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /*JFrame frame = new JFrame("Home");
+                frame.setContentPane(new Home().Accedi);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);*/
+            }
+        });
     }
 }
