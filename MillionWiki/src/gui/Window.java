ee31a4d80b47b29a13ff032ff095b95d57db6a4b
MaxDamageDevStuff <maxdamagedev@gmail.com>
2024-01-18 16:50:31 +0100

    aggiunta una toolbar + aggiunto un non palette component

--- MillionWiki/src/gui/Window.java
+++ MillionWiki/src/gui/Window.java
@@ -4,6 +4,15 @@ import javax.swing.*;
 
 public class Window {
     private JPanel mainPanelWindow;
+    private JPanel toolbarPanel;
+    private JPanel windowPane;
+    private JButton homeButton;
+    private JTextField textField1;
+    private JButton loginButton;
+    private JButton registerButton;
+    private JPanel mainToolbar;
+    private JPanel unloggedUserPanel;
+
     public Window(){
         new Home();
     }
