45e7b062bd3c134306571d8fdc8fb68cc5361cbe
MaxDamageDevStuff <maxdamagedev@gmail.com>
2024-01-23 13:52:23 +0100

    fix e modularizzazione interfaccia
    questa modifica è soltanto estetica, e l'interfaccia è attualmente non funzionante

---
+++ MillionWiki/src/gui/Toolbar.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class Toolbar {
+    private JPanel mainToolbar;
+    private JButton homeButton;
+    private JTextField textField1;
+    private JPanel unloggedUserPanel;
+    private JButton loginButton;
+    private JButton registerButton;
+    private JPanel Toolbar;
+}
--- MillionWiki/src/gui/Window.java
+++ MillionWiki/src/gui/Window.java
@@ -5,17 +5,10 @@ import javax.swing.*;
 public class Window {
     private JPanel mainPanelWindow;
     private JPanel toolbarPanel;
-    private JPanel mainToolbar;
-    private JPanel unloggedUserPanel;
     private JPanel windowPane;
-    
-    private JButton homeButton;
-    private JTextField textField1;
-    private JButton loginButton;
-    private JButton registerButton;
 
     public Window(){
-        JFrame frame = new JFrame("Home");
+        JFrame frame = new JFrame("Million Wiki");
         frame.setContentPane(mainPanelWindow);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
