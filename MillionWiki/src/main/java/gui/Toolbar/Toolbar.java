8c8cb399c78dfb90ab4590edbc40bdf59cac67be
MaxDamageDevStuff <68134538+MaxDamageDevStuff@users.noreply.github.com>
2024-02-29 17:31:27 +0100

    unlogged toolbar spostata in un nuovo form

---
+++ MillionWiki/src/main/java/gui/Toolbar/Toolbar.java
@@ -0,0 +1,28 @@
+package gui.Toolbar;
+import gui.Window;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Toolbar {
+    private JPanel commonToolbar;
+    private JButton homeBtn;
+    private JTextField searchTxtFld;
+    private JPanel accessUserPanel;
+    private JPanel mainPanelToolbar;
+    private Window window;
+    public Toolbar() {
+        homeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                window = Window.checkWindow(window, mainPanelToolbar);
+                window.switchPanel(window.getHomePanel());
+            }
+        });
+
+    }
+    public JPanel getPanel() {
+        return mainPanelToolbar;
+    }
+}
