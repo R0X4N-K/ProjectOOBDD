a8c132dc957603628d8426ead6184df999fe86fa
Sarnyxq <gennaro.sarnataro8@gmail.com>
2024-03-05 18:59:23 +0100

    1. Aggiunto popupMenu delle notifiche a LoggedToolbar

--- MillionWiki/src/main/java/gui/toolbar/LoggedToolbar.java
+++ MillionWiki/src/main/java/gui/toolbar/LoggedToolbar.java
@@ -4,9 +4,15 @@ import javax.swing.*;
 
 public class LoggedToolbar {
     private JPanel loggedUserPanel;
-    private Notifications notifications1;
+    private JButton showNotificationsButton;
+    private Notifications notifications = new Notifications();
 
     public LoggedToolbar(){
+        JPopupMenu popupMenu = new JPopupMenu();
+        popupMenu.add(notifications.getPanel());
+        showNotificationsButton.addActionListener(e -> {
+            popupMenu.show(showNotificationsButton, showNotificationsButton.getWidth() / 6, showNotificationsButton.getHeight() / 2);
+        });
     }
     public JPanel getPanel() {
         return loggedUserPanel;
