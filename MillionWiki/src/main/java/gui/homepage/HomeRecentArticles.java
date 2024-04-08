bd2b89c84b76f91e10259de6f4b66e9d65860abb
Giovanni <69719606+R0X4N-K@users.noreply.github.com>
2024-04-08 17:46:08 +0200

    Creata funzione initRecentArticles in HomeRecentArticles che  viene lanciata su un altro thread in modo da non rallentare l'avvio dell'applicazione

--- MillionWiki/src/main/java/gui/homepage/HomeRecentArticles.java
+++ MillionWiki/src/main/java/gui/homepage/HomeRecentArticles.java
@@ -8,10 +8,20 @@ import java.util.ArrayList;
 
 public class HomeRecentArticles {
     private JPanel homeRecentArticlesMainPanel;
+    private Thread thread;
 
     public HomeRecentArticles() {
         homeRecentArticlesMainPanel.setLayout(new GridLayout(2, 5));
+        if (thread == null || !thread.isAlive()) {
+            thread = new Thread(() -> {
+                initRecentArticles();
+            });
+        }
+        thread.setDaemon(true);
+        thread.start();
+    }
 
+    private void initRecentArticles() {
         ArrayList<Integer> recentArticles = new ArrayList<>();
         int attempts = 0;
         while (recentArticles.size() < 10 && attempts < 5) {
@@ -32,7 +42,8 @@ public class HomeRecentArticles {
             HomeArticlePanel recentArticle = new HomeArticlePanel(Controller.getArticlesById(articleId).getTitle(), plainText, articleId);
             homeRecentArticlesMainPanel.add(recentArticle);
         }
-
+        homeRecentArticlesMainPanel.repaint();
+        homeRecentArticlesMainPanel.revalidate();
     }
 
     public JPanel getPanel() {
