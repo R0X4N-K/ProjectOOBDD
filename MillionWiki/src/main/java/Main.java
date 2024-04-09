4ff990fd2eb2b875ca624d4994d4370426d98a55
R0X4N <giovannizapparella002@gmail.com>
2024-04-09 17:22:41 +0200

    Aggiunto FlatLaf (un Look and Feel cross platform e open-source) per rendere più moderna l'interfaccia grafica

--- MillionWiki/src/main/java/Main.java
+++ MillionWiki/src/main/java/Main.java
@@ -1,8 +1,15 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.ui.FlatButtonUI;
 import controller.Controller;
 import gui.*;
 import implementationDAO.ArticleDAOImplementation;
 import implementationDAO.ArticleVersionDAOImplementation;
 import implementationDAO.AuthorDAOImplementation;
+
+import javax.swing.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -12,6 +19,8 @@ public class Main {
         /*Controller controller = new Controller();
         controller.getArticleByTitle("prova");*/
         //Session.getInstance();
+        FlatIntelliJLaf.setup();
+
         if (Controller.verifyAppIstances()) {
             Controller.notifyOtherAppIstances();
         } else {
